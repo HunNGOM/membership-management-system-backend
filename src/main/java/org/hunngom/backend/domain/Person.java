b581eda19dd64f68ed54985e060251a2d5262dfa
Gabor Nagypal <nagypalg@gmail.com>
2019-11-01 12:01:23 +0100

    Added Lombok, added first draft domain class

---
+++ src/main/java/org/hunngom/backend/domain/Person.java
@@ -0,0 +1,10 @@
+package org.hunngom.backend.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private long id;
+    private String name;
+    private int birthDay;
+}
--- src/main/java/org/hunngom/backend/greeting/Greeting.java
+++ src/main/java/org/hunngom/backend/greeting/Greeting.java
@@ -1,20 +1,12 @@
 package org.hunngom.backend.greeting;
 
+import lombok.Data;
+
+@Data
 public class Greeting {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
